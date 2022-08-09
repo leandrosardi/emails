110aa89c4a1707d18e12c93f515cd29019f211cd
leandrosardi <leandro.sardi@expandedventure.com>
2022-08-09 15:55:42 -0300

    #5 #6 #7

--- models/skeleton/address.rb
+++ models/skeleton/address.rb
@@ -3,12 +3,112 @@ module BlackStack
         class Address < Sequel::Model(:eml_address)
             many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
             many_to_one :shared, :class=>:'BlackStack::MySaaS::Account', :key=>:shared_id_account
-            
             # types
             TYPE_GMAIL = 0
-            TYPE_YAHOO = 1
-            TYPE_HOTMAIL = 2
-            
+            TYPE_YAHOO = 1 # pending to develop
+            TYPE_HOTMAIL = 2 # pending to develop
+
+            # send email.
+            # this is a general purpose method to send email.
+            # end user should not call this method.
+            def send(to, subject, body, from_name, reply_to)
+                raise 'Abstract method'                
+            end # send
+
+            def send(h)
+                err = []
+                # validate: h is a hash
+                err << 'h is not a hash' unless h.is_a?(Hash)
+                # validate: to is required
+                err << 'to is required' unless h[:to]
+                # validate: subject is required
+                err << 'subject is required' unless h[:subject]
+                # validate: body is required
+                err << 'body is required' unless h[:body]
+                # validate: from_name is required
+                err << 'from_name is required' unless h[:from_name]
+                # validate: reply_to is required
+                err << 'reply_to is required' unless h[:reply_to]
+                # validate: to is a string and it is a valid email address
+                err << 'to is not a string' unless !h[:to].nil? && !h[:to].is_a?(String)
+                err << 'to is not a valid email address' unless !h[:to].nil? && !h[:to].to_s.email?
+                # validate: subject is a string
+                err << 'subject is not a string' unless !h[:subject].nil? && !h[:subject].is_a?(String)
+                # validate: body is a string
+                err << 'body is not a string' unless !h[:body].nil? && !h[:body].is_a?(String)
+                # validate: from_name is a string
+                err << 'from_name is not a string' unless !h[:from_name].nil? && !h[:from_name].is_a?(String)
+                # validate: reply_to is a string and it is a valid email address
+                err << 'reply_to is not a string' unless !h[:reply_to].nil? && !h[:reply_to].is_a?(String)
+                err << 'reply_to is not a valid email address' unless !h[:reply_to].nil? && !h[:reply_to].to_s.email?
+                # return if errors
+                return err if err.size > 0
+                # send email
+                send(h[:to], h[:subject], h[:body], h[:from_name], h[:reply_to])
+            end
+
+
         end # class Address
+
+
+        class GMail < BlackStack::Emails::Address
+            # authentication token file
+            def token
+                token = "#{self.user.account.storage_sub_folder('emails.google.tokens')}/#{id}.yaml".freeze
+            end # def token
+
+            # to access the gmail account, we need to use the gmail api's credentials
+            def credentials
+                oob_uri = BlackStack::Emails::Google::oob_uri
+                app_name = BlackStack::Emails::Google::app_name
+                google_api_certificate = BlackStack::Emails::Google::google_api_certificate
+                scope = BlackStack::Emails::Google::scope 
+
+                client_id = Google::Auth::ClientId.from_file google_api_certificate
+                token_store = Google::Auth::Stores::FileTokenStore.new file: self.token
+                authorizer = Google::Auth::UserAuthorizer.new client_id, scope, token_store
+                user_id = 'default'
+                ret = authorizer.get_credentials user_id
+                raise "Address credentials not found" if ret.nil?
+                ret
+            end
+
+            # get gmail service
+            def service
+                app_name = BlackStack::Emails::Google::app_name
+                service = Google::Apis::GmailV1::GmailService.new
+                service.client_options.application_name = app_name
+                service.authorization = self.credentials
+                service
+            end # def service
+
+            # send email.
+            # this is a general purpose method to send email.
+            # this should not call this method.
+            def send(to, subject, body, from_name, reply_to)
+                user_id = "me"
+                message = Mail.new(body)
+                message.to = to
+                message.from = "#{from_name} <#{self.address}>"
+                message.reply_to = reply_to
+                message.subject = subject
+                message.text_part = body
+                message.html_part = body
+                self.service.send_user_message(user_id, upload_source: StringIO.new(message.to_s), content_type: 'message/rfc822')                
+            end # send
+
+            # send a test email to the logged in user
+            def send_test(campaign, lead, user)
+                self.send({
+                    :to => user.email, 
+                    :subject => campaign.merged_subject(lead), 
+                    :body => campaign.merged_body(lead), 
+                    :from_name => user.name, 
+                    :reply_to => user.email,
+                })
+            end
+
+        end # class GMail
+
     end # Emails
 end # BlackStack
--- models/skeleton/campaign.rb
+++ models/skeleton/campaign.rb
@@ -82,6 +82,73 @@ module BlackStack
                 t = self.stat_opened
                 t == 0 ? 0 : ((self.stat_unsubscribed.to_f / t.to_f) * 100.to_f).to_i
             end
+
+            # replace merge-tags in the string s with the values of the lead's atrtibutes.
+            # return the string with the merge-tags replaced.
+            # this is a general purpose method to send email.
+            # this should not call this method.
+            def merge(s, lead)
+                ret = s
+                email = lead.datas.select { |d| d.type == Leads::FlData::TYPE_EMAIL }.first
+                phone = lead.datas.select { |d| d.type == Leads::FlData::TYPE_PHONE }.first
+                linkd = lead.datas.select { |d| d.type == Leads::FlData::TYPE_LINKEDIN }.first
+                unsub = "#{CS_HOME_WEBSITE}/api1.0/emails/unsubscribe.json?lid=#{lead.id.to_guid}&gid=#{self.id.to_guid}"
+
+                # replace merge-tags with no fallback values
+                ret.gsub!(/#{Regexp.escape('{company-name}')}/, lead.stat_company_name.to_s)
+                ret.gsub!(/#{Regexp.escape('{first-name}')}/, lead.first_name.to_s)
+                ret.gsub!(/#{Regexp.escape('{last-name}')}/, lead.last_name.to_s)
+                ret.gsub!(/#{Regexp.escape('{location}')}/, lead.stat_location_name.to_s)
+                ret.gsub!(/#{Regexp.escape('{industry}')}/, lead.stat_industry_name.to_s)
+                ret.gsub!(/#{Regexp.escape('{email-address}')}/, email.to_s)
+                ret.gsub!(/#{Regexp.escape('{phone-number}')}/, phone.to_s)
+                ret.gsub!(/#{Regexp.escape('{linkedin-url}')}/, linkd.to_s)
+                ret.gsub!(/#{Regexp.escape('{unsubscribe-url}')}/, unsub.to_s)
+
+                # replace merge-tags with fallback values
+                ret.scan(/#{Regexp.escape('{company-name|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, lead.stat_company_name.to_s.empty? ? m.gsub(/^\{company-name\|/, '').gsub(/\}$/) : lead.stat_company_name.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{first-name|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, lead.first_name.to_s.empty? ? m.gsub(/^\{first-name\|/, '').gsub(/\}$/) : lead.first_name.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{last-name|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, lead.last_name.to_s.empty? ? m.gsub(/^\{last-name\|/, '').gsub(/\}$/) : lead.last_name.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{location|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, lead.stat_location_name.to_s.empty? ? m.gsub(/^\{location\|/, '').gsub(/\}$/) : lead.stat_location_name.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{industry|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, lead.stat_industry_name.to_s.empty? ? m.gsub(/^\{industry\|/, '').gsub(/\}$/) : lead.stat_industry_name.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{email-address|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, email.to_s.empty? ? m.gsub(/^\{email-address\|/, '').gsub(/\}$/) : email.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{phone-number|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, phone.to_s.empty? ? m.gsub(/^\{phone-number\|/, '').gsub(/\}$/) : phone.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{linkedin-url|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, linkd.to_s.empty? ? m.gsub(/^\{linkedin-url\|/, '').gsub(/\}$/) : linkd.to_s)
+                } 
+                ret.scan(/#{Regexp.escape('{unsubscribe-url|')}.*#{Regexp.escape('}')}/).each { |m| 
+                    ret.sub!(m, unsub.to_s.empty? ? m.gsub(/^\{unsubscribe-url\|/, '').gsub(/\}$/) : unsub.to_s)
+                }
+                # return
+                ret             
+            end
+
+            # replace merge-tags in the subject with the values of the lead's atrtibutes.
+            # return the subject with the merge-tags replaced.
+            def merged_subject(lead)
+                merge(self.subject, lead)
+            end
+
+            # replace merge-tags in the body with the values of the lead's atrtibutes.
+            # return the body with the merge-tags replaced.
+            def merged_body(lead)
+                merge(self.body, lead)
+            end
+
         end # class Address
     end # Emails
 end # BlackStack
