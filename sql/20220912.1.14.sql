672839769c49eae8b799ca2a7c2a3b5146e9c958
leandrosardi <leandro.sardi@expandedventure.com>
2022-09-12 15:57:04 -0300

    #14

---
+++ sql/20220912.1.14.sql
@@ -0,0 +1,23 @@
+/*
+select *
+from eml_address
+order by create_time desc
+*/
+
+-- https://github.com/leandrosardi/emails/issues/14
+
+-- message_id of delivered email.
+alter table eml_delivery add message_id varchar(500) null;
+
+-- create a id_conversation for a delivery when it receives the first response.
+alter table eml_delivery add id_conversation uuid null;
+
+-- record responses from leads in the delivery table
+alter table eml_delivery add is_response boolean not null default false;
+
+-- deliveries from manually written emails don't require id_job
+alter table eml_delivery alter column id_job set not null;
+
+-- deliveries from manually written emails don't require subject
+alter table eml_delivery alter column "subject" set not null;
+
