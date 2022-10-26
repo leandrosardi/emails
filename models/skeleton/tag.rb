91f4da6c44c3acb7a88920404703bd700980d148
leandrosardi <leandro.sardi@expandedventure.com>
2022-10-26 12:15:13 -0300

    #50

--- models/skeleton/tag.rb
+++ models/skeleton/tag.rb
@@ -2,6 +2,17 @@ module BlackStack
     module Emails
         class Tag < Sequel::Model(:eml_tag)
             many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
+
+            COLORS = ['orange','yellow','blue','purple','black']
+
+            def self.color(name)
+                COLORS[name.hash % COLORS.length]
+            end
+
+            def color
+                Tag.color(name)
+            end
+
         end # class Tag
     end # Emails
 end # BlackStack
