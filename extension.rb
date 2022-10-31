7ccf1971e8cb680f9480d2fbb0ea9199cc22477c
leandrosardi <leandro.sardi@expandedventure.com>
2022-10-31 09:26:40 -0300

    adding email_verifier to the list of gems to install by this extension

--- extension.rb
+++ extension.rb
@@ -16,11 +16,11 @@ BlackStack::Extensions::add ({
     # what is the section to add this extension in either the top-bar, the footer, the dashboard.
     :services_section => 'Services',
     # show this extension as a service in the top bar?
-    :show_in_top_bar => true,
+    :show_in_top_bar => false,
     # show this extension as a service in the footer?
-    :show_in_footer => true,
+    :show_in_footer => false,
     # show this extension as a service in the dashboard?
-    :show_in_dashboard => true,
+    :show_in_dashboard => false,
 
     # what are the screens to add in the leftbar
     :leftbar_icons => [
@@ -53,6 +53,7 @@ BlackStack::Extensions::add ({
                 gem install --no-document email_reply_parser -v 0.5.10;
                 gem install --no-document csv -v 3.2.2;
                 gem install --no-document verify_email_addresses -v 0.1.0;
+                gem install --no-document email_verifier -v 0.1.0;
             ",
             :sudo => true,
         }],
